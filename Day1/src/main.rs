55e9dd5fae28a4584f71d583d8f3e678b481b2f7
Colthor <github@colthor.co.uk>
2023-12-02 16:43:45 +0000

    Enable clippy/rustfmt

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,29 +1,31 @@
 use std::fs;
 
 const NUMSTRS: &[(&str, &str, usize, &str)] = &[
-        ("one", "1  ", 1, "1"),
-        ("two", "2  ", 2, "2"),
-        ("three", "3    ", 3, "3"),
-        ("four", "4   ", 4, "4"),
-        ("five", "5   ", 5, "5"),
-        ("six", "6  ", 6, "6"),
-        ("seven", "7    ", 7, "7"),
-        ("eight", "8    ", 8, "8"),
-        ("nine", "9   ", 9, "9"),
-    ];
+    ("one", "1  ", 1, "1"),
+    ("two", "2  ", 2, "2"),
+    ("three", "3    ", 3, "3"),
+    ("four", "4   ", 4, "4"),
+    ("five", "5   ", 5, "5"),
+    ("six", "6  ", 6, "6"),
+    ("seven", "7    ", 7, "7"),
+    ("eight", "8    ", 8, "8"),
+    ("nine", "9   ", 9, "9"),
+];
 
 fn first(in_str: &str) -> i32 {
-    let mut v = vec![usize::MAX; 10];
+    let mut v = [usize::MAX; 10];
     for n in NUMSTRS {
         let r_str = in_str.replace(n.0, n.1);
         v[n.2] = r_str.find(n.3).unwrap_or(usize::MAX);
     }
     //println!("{:?}", v);
     let first_index = *v.iter().min().unwrap();
-    i32::try_from(v.iter().position(|x|  *x == first_index ).unwrap()).ok().unwrap()
+    i32::try_from(v.iter().position(|x| *x == first_index).unwrap())
+        .ok()
+        .unwrap()
 }
 fn last(in_str: &str) -> i32 {
-    let mut v = vec![usize::MIN; 10];
+    let mut v = [usize::MIN; 10];
     for n in NUMSTRS {
         let r_str = in_str.replace(n.0, n.1);
         let found = r_str.rfind(n.3);
@@ -34,12 +36,16 @@ fn last(in_str: &str) -> i32 {
     }
     //println!("{:?}", v);
     let first_index = *v.iter().max().unwrap();
-    i32::try_from(v.iter().position(|x|  *x == first_index ).unwrap()).ok().unwrap()
+    i32::try_from(v.iter().position(|x| *x == first_index).unwrap())
+        .ok()
+        .unwrap()
 }
 
 fn main() {
     println!("Hello, world!");
-    let filename = std::env::args().nth(1).expect("Pass filename as command line argument");
+    let filename = std::env::args()
+        .nth(1)
+        .expect("Pass filename as command line argument");
 
     println!("Filename: {}", filename);
 
