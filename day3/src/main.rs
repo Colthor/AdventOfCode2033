6931010229481f9077daf3c820c613a3038802e2
Colthor <github@colthor.co.uk>
2023-12-13 23:42:31 +0000

    Day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::cmp::{max, min};
+use std::{collections::HashSet, fs};
 
 #[derive(Debug, Clone, Copy)]
 struct Schematic {
@@ -6,24 +7,50 @@ struct Schematic {
     is_number: bool,
     is_symbol: bool,
     is_adjacent: bool,
+    num_num: Option<usize>,
 }
 
-fn map_row_to_string(i: &Vec<Schematic>) -> String {
-    let mut out = String::new();
+fn map_to_nums(map: &mut Vec<Vec<Schematic>>) -> Vec<u32> {
+    let mut out: Vec<u32> = vec![]; // = String::new();
+    let mut cn = String::new();
+    let mut count: usize = 0;
     let mut was_num = false;
-
-    for s in i {
-        if s.is_number && (s.is_adjacent || was_num) {
-            out.push(s.c);
-            was_num = true;
-        } else if was_num {
-            out.push(' ');
-            was_num = false;
+    for r in map {
+        for s in r {
+            if s.is_number && (s.is_adjacent || was_num) {
+                //out.push(s.c);
+                cn.push(s.c);
+                was_num = true;
+                s.num_num = Some(count);
+            } else if was_num {
+                //out.push(' ');
+                was_num = false;
+                match cn.parse::<u32>() {
+                    Ok(x) => {
+                        out.push(x);
+                        println!("Number {count} = {x}");
+                        count += 1;
+                    }
+                    Err(e) => println!("Parse failed: {cn}: {e}"),
+                }
+                cn.clear();
+            }
         }
+        was_num = false;
+        match cn.parse::<u32>() {
+            Ok(x) => {
+                out.push(x);
+                println!("Number {count} = {x}");
+                count += 1;
+            }
+            Err(e) => println!("Parse failed: {cn}: {e}"),
+        }
+        cn.clear();
     }
     out
 }
 
+/*
 fn sum_of_numbers_in_string(i: String) -> u32 {
     let mut out: u32 = 0;
 
@@ -33,6 +60,56 @@ fn sum_of_numbers_in_string(i: String) -> u32 {
             Err(e) => println!("Parse failed: {n}: {e}"),
         }
     }
+    out
+}*/
+
+fn adjacent_nums(
+    map: &Vec<Vec<Schematic>>,
+    rows: usize,
+    cols: usize,
+    r_c: usize,
+    c_c: usize,
+) -> HashSet<usize> {
+    let mut out = HashSet::<usize>::new();
+
+    /*println!(
+        "rows: {}..{}, cols: {}..{}",
+        max(0, r_c - 1),
+        min(rows, r_c + 1),
+        max(0, c_c - 1),
+        min(cols, c_c + 1)
+    );*/
+
+    for r in max(0, r_c - 1)..min(rows, r_c + 2) {
+        for c in max(0, c_c - 1)..min(cols, c_c + 2) {
+            //println!("check: {r},{c}");
+            if let Some(x) = map[r][c].num_num {
+                println!("beep: {r},{c}: {x}");
+                out.insert(x);
+            }
+        }
+    }
+    out
+}
+
+fn calculate_gears(map: &Vec<Vec<Schematic>>, nums: Vec<u32>, rows: usize, cols: usize) -> u32 {
+    let mut out: u32 = 0;
+
+    for r in 0..rows {
+        for c in 0..cols {
+            if map[r][c].is_symbol {
+                println!("Symbol? {r},{c}");
+                let adj_nums = adjacent_nums(map, rows, cols, r, c);
+                if adj_nums.len() == 2 {
+                    println!("Gear! {r},{c}");
+                    let mut i = adj_nums.iter();
+                    out += nums[*i.next().expect("First item in adj_nums")]
+                        * nums[*i.next().expect("second item in adj_nums")];
+                }
+            }
+        }
+    }
+
     out
 }
 
@@ -56,6 +133,7 @@ fn main() {
                 is_number: false,
                 is_symbol: false,
                 is_adjacent: true,
+                num_num: None,
             };
             height
         ]);
@@ -102,11 +180,17 @@ fn main() {
         }
     }
     // Map is read and labelled now
-    let mut result: u32 = 0;
-    for l in map {
-        let rowstr = map_row_to_string(&l);
+    let nums = map_to_nums(&mut map);
+    let result: u32 = nums.iter().sum();
+    println!("{:#?}", nums);
+    /*for l in map {
+        let rowstr = map_row_to_nums(&l);
         println!("{}", rowstr);
         result += sum_of_numbers_in_string(rowstr);
-    }
+    }*/
     println!("Result: {result}");
+    let gears = calculate_gears(&map, nums, width, height);
+    println!("Gears: {gears}");
 }
+
+//wrong: 1265119781
